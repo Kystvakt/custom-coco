fa39df99416135480ce673e46643a78338104666
Kystvakt <reconfiguration@protonmail.com>
2023-04-11 17:37:58 +0900

    Update to display inference coordinates

--- inference.py
+++ inference.py
@@ -1,6 +1,8 @@
 import glob
 
+import numpy as np
 import torch.cuda
+import matplotlib.pyplot as plt
 from PIL import Image
 from torch.optim import SGD
 from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2
@@ -14,27 +16,51 @@ else:
     device = 'cpu'
 
 # Load model
-model = fasterrcnn_resnet50_fpn_v2(pretrained=False, num_classes=2)
+model = fasterrcnn_resnet50_fpn_v2(num_classes=2)
 model.to(device)
 optimizer = SGD(model.parameters(), lr=0.001, momentum=0.9)
 
 # Load checkpoint
-checkpoint = torch.load('checkpoint/checkpoint_epoch_12.pth')
+checkpoint = torch.load('checkpoint/best_checkpoint.pth')
 model.load_state_dict(checkpoint['model_state_dict'])
 optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 epoch = checkpoint['epoch']
 model.eval()
 
 # Inference
-filelist = glob.glob('*.jpg', root_dir='../data/train/image/')
-for img in filelist:
-    x = Image.open('./data/train/image/' + img)
-    transform = Compose([
-        ToTensor(),
-        Resize((512, 384), antialias=True),
-    ])
-    x = transform(x)
-    x = x.unsqueeze(0).to(device)
-    y_hat = model(x)
+filelist = glob.glob('*.jpg', root_dir='data/train/image/')
 
-    print(y_hat)
+img_idx = 0
+img_path = 'data/train/image/' + filelist[img_idx]
+
+x = Image.open(img_path)
+transform = Compose([
+    Resize((512, 384), antialias=True),
+    ToTensor(),
+])
+x = transform(x)
+x = x.unsqueeze(0).to(device)
+output = model(x)
+y = np.array(x.cpu())
+y = np.transpose(y[0], (1, 2, 0))
+plt.imshow(y)
+for (x1, y1, x2, y2) in output[0]['boxes']:
+    x1 = x1.cpu().detach().numpy()
+    y1 = y1.cpu().detach().numpy()
+    x2 = x2.cpu().detach().numpy()
+    y2 = y2.cpu().detach().numpy()
+    plt.plot([x1, x2], [y1, y2])
+    # plt.scatter(x1, y1)
+    # plt.scatter(x2, y2)
+plt.show()
+# for img in filelist:
+#     x = Image.open('./data/train/image/' + img)
+#     transform = Compose([
+#         ToTensor(),
+#         Resize((512, 384), antialias=True),
+#     ])
+#     x = transform(x)
+#     x = x.unsqueeze(0).to(device)
+#     y_hat = model(x)
+#
+#     print(y_hat.keys())
