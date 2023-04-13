17b91dd7ce78cd7c6f9de356c3fb1838dd128fd7
Kystvakt <reconfiguration@protonmail.com>
2023-04-13 14:31:49 +0900

    Update sample run and continue from checkpoint

--- train.py
+++ train.py
@@ -1,14 +1,14 @@
 import argparse
 import os
-import wandb
 
+import numpy as np
 import torch
-from torch.utils.data import DataLoader
+import wandb
+from torch.utils.data import DataLoader, Subset
 from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2
-from tqdm import tqdm, trange
 
 from dataset import CustomCocoDataset, ResizeWithBoxes, ToTensorWithBoxes, CustomCompose
-from utils import save_checkpoint
+from utils import save_checkpoint, progressbar
 
 
 def collate_fn(batch):
@@ -19,9 +19,11 @@ def main():
     # Arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--use-wandb", action="store_true")
+    parser.add_argument("--sample", action="store_true")
     parser.add_argument("--batch-size", default=8, type=int)
     parser.add_argument("--num-epochs", default=50, type=int)
     parser.add_argument("--lr", default=0.005, type=float)
+    parser.add_argument("--continue-from", default=None, type=str, help="checkpoint file path")
     args = parser.parse_args()
 
     # Logger
@@ -33,11 +35,17 @@ def main():
 
     # Data
     transform = CustomCompose([
-        ResizeWithBoxes((512, 384)),
+        ResizeWithBoxes((384, 512)),  # takes (height, width)
         ToTensorWithBoxes(),
     ])
     tr_ds = CustomCocoDataset(image_path='./data/train/image/', annot_path='./data/train.json', transform=transform)
     te_ds = CustomCocoDataset(image_path='./data/test/image/', annot_path='./data/test.json', transform=transform)
+    if args.sample:
+        tr_ds = Subset(tr_ds, np.arange(1))
+        te_ds = Subset(te_ds, np.arange(1))
+        args.batch_size = 1
+        args.num_epochs = 200
+
     tr_dl = DataLoader(
         tr_ds,
         batch_size=args.batch_size,
@@ -54,21 +62,23 @@ def main():
     )
     print("Train dataset size:", len(tr_ds))
     print("Validation dataset size:", len(te_ds))
+
     # CUDA
     torch.set_float32_matmul_precision('high')
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     # Model
-    model = fasterrcnn_resnet50_fpn_v2(
-        # weights_backbone=ResNet50_Weights,
-        # trainable_backbone_layers=3,
-        num_classes=2,
-    )
+    model = fasterrcnn_resnet50_fpn_v2(num_classes=2)
     model.to(device)
     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=0.0005)
+    if args.continue_from is not None:
+        checkpoint = torch.load(args.continue_from)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        # epoch = checkpoint['epoch']
 
     # Train and validation
-    checkpoint_dir = 'checkpoint/'
+    checkpoint_dir = 'checkpoint/best/'
     best_valid_loss = float('inf')
 
     for epoch in range(args.num_epochs):
@@ -77,10 +87,10 @@ def main():
         valid_loss = 0.
 
         # train
-        for images, targets in tqdm(tr_dl):
+        # TODO: implement learning rate scheduler
+        for batch_idx, (images, boxes) in enumerate(tr_dl):
             images = list(img.to(device) for img in images)
 
-            boxes = targets
             targets = list()
             for box in boxes:
                 target = {
@@ -89,21 +99,29 @@ def main():
                 }
                 targets.append(target)
 
+            # training losses
             loss_dict = model(images, targets)
-            losses = sum(loss for loss in loss_dict.values())  # weights are all 1.0
+            train_loss_classifier = loss_dict['loss_classifier']
+            train_loss_box_reg = loss_dict['loss_box_reg']
+            train_loss_objectness = loss_dict['loss_objectness']
+            train_loss_rpn_box_reg = loss_dict['loss_rpn_box_reg']
+
+            losses = sum(loss for loss in loss_dict.values())
 
             optimizer.zero_grad()
             losses.backward()
             optimizer.step()
 
+            # progress bar
             train_loss += losses.item()
+            log = f"Train Loss: {losses.item():.3f}"
+            progressbar(batch_idx, len(tr_dl), log)
 
         train_loss = train_loss / len(tr_dl)
-        print(train_loss)
 
         # eval
         with torch.no_grad():
-            for images, boxes in tqdm(te_dl):
+            for batch_idx, (images, boxes) in enumerate(te_dl):
                 images = list(img.to(device) for img in images)
 
                 targets = list()
@@ -114,17 +132,38 @@ def main():
                     }
                     targets.append(target)
 
+                # validation losses
                 loss_dict = model(images, targets)
+                valid_loss_classifier = loss_dict['loss_classifier']
+                valid_loss_box_reg = loss_dict['loss_box_reg']
+                valid_loss_objectness = loss_dict['loss_objectness']
+                valid_loss_rpn_box_reg = loss_dict['loss_rpn_box_reg']
+
                 losses = sum(loss for loss in loss_dict.values())
 
+                # progress bar
                 valid_loss += losses.item()
+                log = f"Valid Loss: {losses.item():.3f}"
+                progressbar(batch_idx, len(te_dl), log)
 
             valid_loss = valid_loss / len(te_dl)
-            print(valid_loss)
 
+        # logging
         if args.use_wandb:
-            wandb.log({"train-loss": train_loss, "valid-loss": valid_loss, "epoch": epoch + 1})
+            wandb.log({
+                "train_loss_classifier": train_loss_classifier,
+                "train_loss_box_reg": train_loss_box_reg,
+                "train_loss_objectness": train_loss_objectness,
+                "train_loss_rpn_box_reg": train_loss_rpn_box_reg,
+                "valid_loss_classifier": valid_loss_classifier,
+                "valid_loss_box_reg": valid_loss_box_reg,
+                "valid_loss_objectness": valid_loss_objectness,
+                "valid_loss_rpn_box_reg": valid_loss_rpn_box_reg
+            })
 
+        # save checkpoint
+        ckptpath = os.path.join('checkpoint/', 'checkpoint.pth')
+        save_checkpoint(epoch + 1, model, train_loss, valid_loss, optimizer, filepath=ckptpath)
         if valid_loss < best_valid_loss:
             best_valid_loss = valid_loss
             checkpoint_path = os.path.join(checkpoint_dir, f"best_checkpoint.pth")
